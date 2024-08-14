975bc671be28010a703156ea01b4be5b117f0362
Emma <Emma@fanap.ir>
2024-08-14 18:21:20 +0330

    add game fundmentals

--- src/Board/index.tsx
+++ src/Board/index.tsx
@@ -1,6 +1,23 @@
-import { FC } from "react";
+import { FC, useState } from "react";
+
+interface Circle {
+  x: number;
+  y: number;
+}
 
 const Board: FC = () => {
+  const [circles, setCircles] = useState<Circle[]>([]);
+  const handleClick = (e: MouseEvent) => {
+    setCircles((prevState: Circle[]) => {
+      return [
+        ...prevState,
+        {
+          x: e.clientX,
+          y: e.clientY,
+        },
+      ];
+    });
+  };
   return <div className="board">Board Game</div>;
 };
 export default Board;
