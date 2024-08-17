6136e1c7523abb1715ada10e5834f634f3017cc4
Emma <Emma@fanap.ir>
2024-08-17 16:03:39 +0330

    feature add circles in the box

--- src/Board/index.tsx
+++ src/Board/index.tsx
@@ -1,23 +1,33 @@
-import { FC, useState } from "react";
+import { FC, useState, MouseEvent } from "react";
+import { COLORS } from "../constants";
+import Circles from "./Circles";
+import { CircleFullType } from "./types";
 
-interface Circle {
-  x: number;
-  y: number;
-}
+const getRandomColor = () => {
+  const randomIndex = Math.floor(Math.random() * COLORS.length);
+  return COLORS[randomIndex];
+};
 
 const Board: FC = () => {
-  const [circles, setCircles] = useState<Circle[]>([]);
-  const handleClick = (e: MouseEvent) => {
-    setCircles((prevState: Circle[]) => {
+  const [circles, setCircles] = useState<CircleFullType[]>([]);
+
+  const handleClick = (e: MouseEvent<HTMLDivElement>) => {
+    setCircles((prevState: CircleFullType[]) => {
       return [
         ...prevState,
         {
           x: e.clientX,
           y: e.clientY,
+          id: +new Date(),
+          bgColor: getRandomColor(),
         },
       ];
     });
   };
-  return <div className="board">Board Game</div>;
+  return (
+    <div className="board" onClick={handleClick}>
+      <Circles circles={circles} />
+    </div>
+  );
 };
 export default Board;
